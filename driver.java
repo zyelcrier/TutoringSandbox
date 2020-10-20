ac08276381bda7625f57de3d7642ca5155008921
abryhiaaa <abryhiadirons@gmail.com>
2020-10-20 16:35:08 -0500

    Update driver.java

---
+++ driver.java
@@ -0,0 +1,14 @@
+public class driver{
+    public static void main(String [] args){
+        HashMap<Integer,ArrayList<String>> token = populateMap(4);
+        System.out.println(token);
+    }//main
+    public static HashMap<Integer,ArrayList<String>> populateMap(int keys) {
+        HashMap <Integer,ArrayList<String>> result = new HashMap<>();
+        for(int i = 0; i < keys; i++){
+            ArrayList<String> temp = new ArrayList<>();
+            result.put(i,temp);
+        }
+        return result;
+    }
+}//class
