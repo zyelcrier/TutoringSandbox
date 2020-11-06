3eeafdf0943b896ba87dd0f9914bc7c71232841b
abryhiaaa <abryhiadirons@gmail.com>
2020-11-06 15:00:40 -0600

    Create recSand.java

---
+++ recSand.java
@@ -0,0 +1,14 @@
+public class recSand{
+    public static void main(String[] args) {
+        System.out.println("Rec Fact: " + recFact(6));
+    }//main
+    public static int recFact(int n){
+        if(n == 1){
+            return 1;
+        }else{
+            System.out.println("Value of n: " + n);
+            System.out.println("Value of n-1: " + (n-1));
+            return n*recFact(n-1);
+        }
+    }
+}//class
